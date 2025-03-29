fb6be10ab2a44760361edd1d776cc18f3e2c7e18
Laurence Blackledge <lwblackledge@gmail.com>
2025-03-29 12:58:40 -0400

    Initial

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+volume = 66
+
+if volume < 20:
+    print("It's kinda quiet")
+elif 20 <= volume < 40:
+    print("It's okay")
+elif 40 <= volume < 60:
+    print("It's pretty loud")
+elif 60 <= volume < 80:
+    print("Holy crap")
+else:
+    print("Shit's loud, bro")
